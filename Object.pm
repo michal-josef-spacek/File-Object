454542baa4427f9fec09bc3109525e56a0130384
Michal Špaček <tupinek@gmail.com>
2011-07-14 14:21:56 +0200

    Fix links in doc.

--- Object.pm
+++ Object.pm
@@ -276,8 +276,8 @@ L<File::Spec::Functions(3pm)>.
 
 =head1 AUTHOR
 
- Michal Špaček L<skim@cpan.org>
- http://skim.cz
+ Michal Špaček L<mailto:skim@cpan.org>
+ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
