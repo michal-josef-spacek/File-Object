3280287eed01116fe27c443f46224080f6868b90
Michal Špaček <tupinek@gmail.com>
2011-07-15 21:52:48 +0200

    Prepare for 0.02 version.

--- Object.pm
+++ Object.pm
@@ -11,7 +11,7 @@ use FindBin qw($Bin $Script);
 use File::Spec::Functions qw(catdir catfile splitdir);
 
 # Version.
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Constructor.
 sub new {
@@ -375,6 +375,6 @@ BSD license.
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
